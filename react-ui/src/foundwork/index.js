fbf08734caedf850aa8a40bcba71135d6586e7ef
adate1 <adate1@asu.edu>
2024-04-18 09:28:31 -0700

    [US267 Task296] Create chart from the data for found work (responsive container)

--- react-ui/src/foundwork/index.js
+++ react-ui/src/foundwork/index.js
@@ -7,10 +7,10 @@ import {
 	Tooltip,
 	CartesianGrid,
 	Legend,
+    ResponsiveContainer
 } from 'recharts';
 
 const FoundWork = ({ foundWorkData }) => {
-    console.log(foundWorkData)
     const tasksByDate = foundWorkData?.reduce((acc, task) => {
         const date = new Date(task.createdDate).toDateString();
         acc[date] = (acc[date] || 0) + 1;
@@ -21,33 +21,40 @@ const FoundWork = ({ foundWorkData }) => {
         count,
     }));    
     return(
-        <div align="center" width={750} height={300} sx={{ mt: 2 }}>
-            <LineChart
-                width={730}
-                height={250}
-                data={chartData}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-            >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-                dataKey="date"
-                tickFormatter={(dateStr) => {
-                    const date = new Date(dateStr);
-                    const month = String(date.getMonth() + 1).padStart(2, '0');
-                    const day = String(date.getDate()).padStart(2, '0');
-                    return `${month}/${day}`;
-                }}
-            ></XAxis>
-            <YAxis></YAxis>
-            <Tooltip />
-            <Legend />
-            <Line
-                type="monotone"
-                dataKey="count"
-                stroke="#8884d8"
-                name="# of tasks added"
-            />
-            </LineChart>
+        <div style={{ textAlign: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+        <div style={{ width: '90%' }}>
+        <h4 style={{ textAlign: 'center' }}>Burndown Chart</h4>
+            <ResponsiveContainer width="100%" height={600}>
+                <LineChart
+                    width={730}
+                    height={250}
+                    data={chartData}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis
+                    dataKey="date"
+                    tickFormatter={(dateStr) => {
+                        const date = new Date(dateStr);
+                        const month = String(date.getMonth() + 1).padStart(2, '0');
+                        const day = String(date.getDate()).padStart(2, '0');
+                        return `${month}/${day}`;
+                    }}
+                ></XAxis>
+                <YAxis></YAxis>
+                <Tooltip />
+                <Legend />
+                <Line
+                    type="monotone"
+                    dataKey="count"
+                    stroke="#8884d8"
+                    name="# of tasks added"
+                />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+        </div>
         </div>
     );
 }
