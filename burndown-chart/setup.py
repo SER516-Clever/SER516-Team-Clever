ceff3901dcdf95f3815a930522893e32cc1b202b
achaur11 <achaur11@asu.edu>
2024-04-19 14:51:13 -0700

    US207 Task309: Adding pipelines for Team Clever

---
+++ burndown-chart/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='curndown-chart',
+    packages=find_packages(),
+    version='1.0',
+    description='microservice for burndown',
+    author='achaur11'
+)
