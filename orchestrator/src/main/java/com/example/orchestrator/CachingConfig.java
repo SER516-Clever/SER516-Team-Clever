0700e49e15fcb349e104c68c18ceb1d6370b314b
PradnyaC11 <pchaud21@asu.edu>
2024-04-19 22:31:41 -0700

    [US 277 Task 322] Add cache config to API gateway

---
+++ orchestrator/src/main/java/com/example/orchestrator/CachingConfig.java
@@ -0,0 +1,35 @@
+package com.example.orchestrator;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("adoptedWorkByMilestone",
+                "adoptedWorkByProject",
+                "foundWork",
+                "DoTbyProject",
+                "DoTbySlug",
+                "DoTBVbyProject",
+                "DoTBVbySlug",
+                "projectList",
+                "projectByID",
+                "projectBySlug",
+                "burnDown",
+                "cycleTime",
+                "leadTime",
+                "cruft",
+                "devFocus",
+                "sprints",
+                "project",
+                "techDebt"
+                );
+    }
+}
